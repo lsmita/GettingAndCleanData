57bc7fe791a5fcb7637bff1da172649deaabf3c4
lsmita <smitalath@gmail.com>
2015-09-27 13:56:01 -0600

    Update run_analysis.R
    1. Update line# 78-80 to escape paranthesis t make it work correctly.
    2. Remove the gsub for "angle"

--- run_analysis.R
+++ run_analysis.R
@@ -75,10 +75,9 @@ updateToDescriptiveNames <- function(extractedData) {
     names(extractedData)<-gsub("^t", "Time", names(extractedData))
     names(extractedData)<-gsub("^f", "Frequency", names(extractedData))
     names(extractedData)<-gsub("tBody", "TimeBody", names(extractedData))
-    names(extractedData)<-gsub("-mean()", "Mean", names(extractedData), ignore.case = TRUE)
-    names(extractedData)<-gsub("-std()", "Std", names(extractedData), ignore.case = TRUE)
-    names(extractedData)<-gsub("-freq()", "Frequency", names(extractedData), ignore.case = TRUE)
-    names(extractedData)<-gsub("angle", "Angle", names(extractedData))
+    names(extractedData)<-gsub("-mean\\(\\)", "Mean", names(extractedData), ignore.case = TRUE)
+    names(extractedData)<-gsub("-std\\(\\)", "Std", names(extractedData), ignore.case = TRUE)
+    names(extractedData)<-gsub("-freq\\(\\)", "Frequency", names(extractedData), ignore.case = TRUE)
     names(extractedData)<-gsub("gravity", "Gravity", names(extractedData))
     names(extractedData)
 }
